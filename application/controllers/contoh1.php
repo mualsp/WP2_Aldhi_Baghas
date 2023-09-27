3b96eb1bf615ed40da42c5d22e317c6c7ebbeaa4
aldhibaghas <mualsp154@gmail.com>
2023-09-27 14:46:00 +0700

    menambahkan contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Aldhi Baghas
+            Saya tinggal di daerah Cibitung
+            olah raga yang saya sukai adalah
+            Badminton";
+    }
+}
